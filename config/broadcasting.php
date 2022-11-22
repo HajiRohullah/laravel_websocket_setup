efc4ae39c009a693532bbca26f113dea185dad0d
HajiRohullah <rohullahhussaini67@gmail.com>
2022-11-22 16:33:21 +0430

    save changes

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,10 +39,17 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
+                'useTLS' => true,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'https'
+                'scheme' => 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ],
+
             ],
+
         ],
 
 
